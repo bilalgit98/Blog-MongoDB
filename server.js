fa6d0ae9cd32856ca69b1c84459ee9b5f60e283a
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-30 23:01:56 +0000

    Creating an index route

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const app = express();
 
 const PORT = 5000;
 
+//setting up view engine to render html
+app.set("view engine", "ejs");
+
+//setting up get request on the root
+app.get("/", (req, res) => {
+  //we use render instead of send.
+  // we set the path to the index file
+  res.render("index");
+});
+
 app.listen(PORT, console.log("Server has started on Port 5000"));
