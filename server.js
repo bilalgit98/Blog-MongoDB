0f5f3f571dc7d4afb2d3225dcc8dd9411f5e4e6b
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-31 15:52:26 +0000

    Creating and Displaying Articles

--- server.js
+++ server.js
@@ -15,8 +15,22 @@ app.get("/", (req, res) => {
   //we use render instead of send.
   // we set the path to the index file
   //we pass the articles from the server to the index file.
-  const articles = [{}];
-  res.render("index", { articles: "hello" });
+  const articles = [
+    {
+      title: "test",
+      dateCreated: 20 / 12 / 2022,
+      description: "this is a description",
+    },
+    {
+      title: "test2",
+      dateCreated: 21 / 11 / 2022,
+      description: "this is a description 2",
+    },
+  ];
+
+  //article is passed to the index file.
+  //article will use the variable  articles to get "articles"
+  res.render("index", { article: articles });
 });
 
 app.listen(PORT, console.log("Server has started on Port 5000"));
